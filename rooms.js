ae798e417d218f28c6f06f521f5b1c5b6976b0c7
Ekaterina Dobrilko <ekaterina.dobrilko@exposit-ds.com>
2016-09-20 17:08:14 +0300

    users and rooms pages done

--- rooms.js
+++ rooms.js
@@ -1,22 +1,124 @@
 import React, {Component, PropTypes} from 'react';
-import {View, Text, TouchableHighlight} from 'react-native';
+import {
+    View,
+    Navigator,
+    Text,
+    Image,
+    TouchableHighlight,
+    TouchableOpacity,
+    AsyncStorage,
+    ListView,
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+var gravatarApi = require('gravatar-api');
 
+var rooms = [];
 export default class Rooms extends Component {
-    navMessages() {
-        this.props.navigator.push({
-            id: 'Messages'
-        });
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state =
+        {
+            rooms: ds.cloneWithRows(rooms)
+        }
+    }
 
+    navUsers() {
+        this.props.navigator.push({
+            id: 'Users'
+        });
+    }
+
+    getRooms() {
+        AsyncStorage.getItem('X-AUTH-TOKEN').then((authToken) => {
+            fetch('http://chat.exposit-ds.com/user/room/all',
+                {
+                    method: 'GET',
+                    headers: {
+                        'X-AUTH-TOKEN': authToken
+                    }
+                }
+            ).then((response) => response.json()).then((responseJson) => {
+                console.log(responseJson['rooms']);
+                this.setState({
+                    rooms: this.state.rooms.cloneWithRows(responseJson['rooms'])
+                });
+                console.log(this.state.rooms);
+            })
+                .catch((error) => {
+                    console.error(error);
+                })
+        });
+    }
+
+    _renderRow(rowData) {
+        console.log('render row working');
+        console.log(rowData.email);
+        return (<TouchableOpacity
+            style = {[styles.listRow]}>
+            <View style = {{flex: 1, justifyContent: 'center', paddingLeft: 10, paddingRight: 10}}>
+                <Text ellipsizeMode = "tail"
+                      style = {{flex:1, justifyContent: 'center', fontSize:18}}>{rowData.name}</Text>
+            </View>
+        </TouchableOpacity> )
     }
 
     render() {
         return (
             <View>
-                <Text>This is rooms page scene!</Text>
-                <TouchableHighlight onPress = {this.navMessages.bind(this)}>
-                    <Text>Go to Messages Screen</Text>
+                <TouchableHighlight underlayColor = "#56a570" style = {[styles.goNextButton]}
+                                    onPress = {this.navUsers.bind(this)}>
+                    <Text style = {[styles.goNextButtonText]}>Go To Users ></Text>
                 </TouchableHighlight>
+                <TouchableHighlight underlayColor = "#56a570" style = {[styles.signInButton]}
+                                    onPress = {this.getRooms.bind(this)}>
+                    <Text style = {[styles.signInButtonText]}>LOAD ROOMS</Text>
+                </TouchableHighlight>
+                <ScrollView>
+                    <ListView style = {{flex: 1, height: 520, flexDirection: 'row'}}
+                              dataSource = {this.state.rooms}
+                              renderRow = {this._renderRow}/>
+                </ScrollView>
             </View>
         )
+
     }
 }
+const styles = StyleSheet.create({
+    roundedProfileImage: {
+        justifyContent: 'flex-start',
+        width: 100,
+        height: 100,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 50
+    },
+    signInButton: {
+        backgroundColor: '#5fba7d',
+        padding: 10
+    },
+    signInButtonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    goNextButton: {
+        backgroundColor: '#ffd47c',
+        padding: 10
+    },
+    goNextButtonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    listRow: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        height: 50,
+        borderBottomColor: '#ededed',
+        borderBottomWidth: 1,
+        padding: 10
+    }
+});
--- users.js
+++ users.js
@@ -1,6 +1,17 @@
 import React, {Component, PropTypes} from 'react';
-import {View, Navigator, Text, TouchableHighlight, AsyncStorage, ListView, ScrollView, StyleSheet} from 'react-native';
-import {Gravatar, GravatarApi} from 'react-native-gravatar';
+import {
+    View,
+    Navigator,
+    Text,
+    Image,
+    TouchableHighlight,
+    TouchableOpacity,
+    AsyncStorage,
+    ListView,
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+var gravatarApi = require('gravatar-api');
 
 var users = [];
 export default class Users extends Component {
@@ -13,8 +24,9 @@ export default class Users extends Component {
 
         }
     }
+   
 
-    navMessages() {
+    navRooms() {
         this.props.navigator.push({
             id: 'Rooms'
         });
@@ -45,27 +57,33 @@ export default class Users extends Component {
 
     _renderRow(rowData) {
         console.log('render row working');
-        return (<View
-            style = {{height:100,borderBottomColor: '#ededed', borderBottomWidth:1, paddingLeft:10, paddingTop:10}}>
-            <View>
-                
+        console.log(rowData.email);
+        return (<TouchableOpacity
+            style = {{flexDirection:'row', flexWrap:'wrap', height:120,borderBottomColor: '#ededed', borderBottomWidth:1, paddingLeft:10, paddingTop:10}}>
+            <View style = {{justifyContent: 'flex-start'}}>
+                <Image style = {[styles.roundedProfileImage]}
+                       source = {{uri:gravatarApi.imageUrl({email:rowData.email})}}/>
             </View>
-            <View>
+            <View style = {{justifyContent: 'center', paddingLeft: 20}}>
                 <Text style = {{fontSize:22}}>{rowData.firstName}</Text>
                 <Text style = {{color: '#666'}}>{rowData.lastName}</Text>
             </View>
-        </View> )
+        </TouchableOpacity> )
     }
 
     render() {
         return (
             <View>
-                <Text>This is users page scene!</Text>
-                <TouchableHighlight onPress = {this.getUsers.bind(this)}>
-                    <Text>Go to Rooms Screen</Text>
+                <TouchableHighlight underlayColor = "#56a570" style = {[styles.goNextButton]}
+                                    onPress = {this.navRooms.bind(this)}>
+                    <Text style = {[styles.goNextButtonText]}>Go To Rooms ></Text>
+                </TouchableHighlight>
+                <TouchableHighlight underlayColor = "#56a570" style = {[styles.signInButton]}
+                                    onPress = {this.getUsers.bind(this)}>
+                    <Text style = {[styles.signInButtonText]}>LOAD USERS</Text>
                 </TouchableHighlight>
                 <ScrollView>
-                    <ListView style = {{flex: 1, height: 578}}
+                    <ListView style = {{flex: 1, height: 500}}
                               dataSource = {this.state.users}
                               renderRow = {this._renderRow}/>
                 </ScrollView>
@@ -76,7 +94,29 @@ export default class Users extends Component {
 }
 const styles = StyleSheet.create({
     roundedProfileImage: {
-        width: 100, height: 100, borderWidth: 3,
-        borderColor: 'white', borderRadius: 50
-    }
+        justifyContent: 'flex-start',
+        width: 100,
+        height: 100,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 50
+    },
+    signInButton: {
+        backgroundColor: '#5fba7d',
+        padding: 10
+    },
+    signInButtonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    goNextButton: {
+        backgroundColor: '#ffd47c',
+        padding: 10
+    },
+    goNextButtonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center'
+    },
 });
